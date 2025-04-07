eb6d95a08b3797c0fc1dfbbc3b2efe0f50aa999c
Suresh Kumar <87288485+sureshkumar-devops@users.noreply.github.com>
2025-04-07 16:39:59 +0530

    Update CouponApplication.java

--- src/main/java/com/w3logics/coupon/CouponApplication.java
+++ src/main/java/com/w3logics/coupon/CouponApplication.java
@@ -2,11 +2,17 @@ package com.w3logics.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CouponApplication {
 
-	
+	   @GetMapping(value = "/index")
+    public String index() {
+        return "index";
+     }   
 	public static void main(String[] args) {
 		SpringApplication.run(CouponApplication.class, args);
 	}
